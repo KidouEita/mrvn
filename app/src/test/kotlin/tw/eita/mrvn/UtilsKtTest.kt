6c5165dfe0c822ce43ac41b8525983a05b1e73cc
Eita <h1537594628@gmail.com>
2022-06-07 14:23:10 +0800

    rewrite test case

--- app/src/test/kotlin/tw/eita/mrvn/UtilsKtTest.kt
+++ app/src/test/kotlin/tw/eita/mrvn/UtilsKtTest.kt
@@ -1,9 +1,8 @@
 package tw.eita.mrvn
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.Assert.*
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,10 +13,15 @@ class UtilsKtTest {
 
     @Test
     fun toReadableTime() {
-        val testLong1 = 1653373800000L
-        val testLong2 = 1653379200000L
+        val actual1 = 1653373800000L
+        val actual2 = 1653379200000L
+
+        val expect1 = "14:30"
+        val expect2 = "16:00"
+
         val timeZone = TimeZone.getTimeZone("Asia/Taipei")
-        assertEquals("14:30", testLong1.toReadableTime(timeZone))
-        assertEquals("16:00", testLong2.toReadableTime(timeZone))
+
+        assertEquals(expect1, actual1.toReadableTime(timeZone))
+        assertEquals(expect2, actual2.toReadableTime(timeZone))
     }
 }
